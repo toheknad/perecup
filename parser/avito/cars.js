f0fd35abd99ff614f6ef6f2883d6c1c32506a8b0
toheknad <yanushkovskyd@gmail.com>
2022-10-20 12:27:51 +0300

    fix

--- parser/avito/cars.js
+++ parser/avito/cars.js
@@ -57,11 +57,6 @@ module.exports = async (url, proxy) => {
     });
     await page.goto(baseUrl);
 
-    const html = await page.content();
-    fs.writeFile('helloworld.html', html, function (err) {
-        if (err) return console.log(err);
-        console.log('Hello World > helloworld.txt');
-    });
 
     await page.evaluate(`
       let ads = document.querySelectorAll('div[class*="items-ads"]');
@@ -71,29 +66,6 @@ module.exports = async (url, proxy) => {
         }
       }
     `);
-    const html2 = await page.content();
-    fs.writeFile('helloworld2.html', html2, function (err) {
-        if (err) return console.log(err);
-        console.log('Hello World > helloworld.txt');
-    });
-    // await page.evaluate(`
-    //   let sel = document.querySelector("${selector1}");
-    //   if (sel) {
-    //     sel.remove()
-    //   }
-    // `);
-    // await page.evaluate(`
-    //   let sel1 = document.querySelector("${selector2}");
-    //   if (sel1) {
-    //     sel1.remove()
-    //   }
-    // `);
-    // await page.evaluate(`
-    //   let sel2 = document.querySelector("${selector3}");
-    //   if (sel2) {
-    //     sel2.remove()
-    //   }
-    // `);
     // await page.screenshot({path: 'buddy-screenshot.png',  fullPage: true });
     // await page.waitForSelector('span[data-marker="pagination-button/next"]');
     //количество страниц≠
