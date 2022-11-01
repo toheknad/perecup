23164f0498765672463a9ac892302907242c64a2
toheknad <yanushkovskyd@gmail.com>
2022-11-01 23:21:03 +0300

    fix

--- backend/src/Module/Telegram/UI/Http/Controller/TelegramController.php
+++ backend/src/Module/Telegram/UI/Http/Controller/TelegramController.php
@@ -25,12 +25,38 @@ class TelegramController extends AbstractController
     #[Route('/telegram/get-messages', name: 'telegram_get_messages', methods: 'GET')]
     public function index(Request $request): JsonResponse
     {
-        $messages = $this->telegram->handleGetUpdates()->getRawData();
-        foreach ($messages['result'] as $message) {
-            $this->messageHandleService->start($message);
+//        $messages = $this->telegram->handleGetUpdates()->getRawData();
+//        foreach ($messages['result'] as $message) {
+//            $this->messageHandleService->start($message);
+//        }
+
+        $data = sprintf(
+            "%s %s %s\n\nHTTP headers:\n",
+            $_SERVER['REQUEST_METHOD'],
+            $_SERVER['REQUEST_URI'],
+            $_SERVER['SERVER_PROTOCOL']
+        );
+
+        $headerList = [];
+        foreach ($_SERVER as $name => $value) {
+            if (preg_match('/^HTTP_/', $name)) {
+                // convert HTTP_HEADER_NAME to Header-Name
+                $name = strtr(substr($name, 5), '_', ' ');
+                $name = ucwords(strtolower($name));
+                $name = strtr($name, ' ', '-');
+
+                // add to list
+                $headerList[$name] = $value;
+            }
         }
 
-        return $this->json(['success'], 200);
+        foreach ($headerList as $name => $value) {
+            $data .= $name . ': ' . $value . "\n";
+        }
+
+        $data .= "\nRequest body:\n";
+
+        return $this->json($data, 200);
     }
 
 }
