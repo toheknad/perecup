0d4d0c637f18863c11ccf67ad580632a074b67ea
toheknad <yanushkovskyd@gmail.com>
2022-11-09 21:44:55 +0100

    fix

--- backend/public/index.php
+++ backend/public/index.php
@@ -11,4 +11,5 @@ file_put_contents('post.txt',  $_POST);
 file_put_contents('request.txt',  $_REQUEST);
 file_put_contents('global.txt',  $_GET);
 error_log("You messed up!", 3, "/var/tmp/my-errors.log");
+error_log("You mess222ed up!");
 //Redirect permanent /payment/yookassa/webhook http://158.160.9.226:8080/payment/yookassa/webhook
