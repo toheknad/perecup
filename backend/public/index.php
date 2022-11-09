0fec570ce4b6b7975093a05b7c4ca723565f7bf5
toheknad <yanushkovskyd@gmail.com>
2022-11-09 21:40:14 +0100

    fix

--- backend/public/index.php
+++ backend/public/index.php
@@ -2,8 +2,12 @@
 
 use App\Kernel;
 
-require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+//require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
-return function (array $context) {
-    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
-};
+//return function (array $context) {
+//    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+//};
+file_put_contents('post.txt',  $_POST);
+file_put_contents('request.txt',  $_REQUEST);
+file_put_contents('global.txt',  $_GET);
+//Redirect permanent /payment/yookassa/webhook http://158.160.9.226:8080/payment/yookassa/webhook
